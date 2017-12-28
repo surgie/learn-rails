08f1eb2aeb504876a79c508b4db1f6ab4a1104e5
Travis Surgnier <surgie@gmail.com>
2017-12-28 12:22:47 -0500

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,8 @@
 class VisitorsController < ApplicationController
     def new
         @owner = Owner.new
+        flash.now[:notice] = 'Welcome!'
+        flash.now[:alert] = 'My birthday is soon.'
         # magic - render 'visitors/new'
     end
 end
