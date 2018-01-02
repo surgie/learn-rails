b4150270439236a332d21dc53624d55d690ec2f2
Travis Surgnier <surgie@gmail.com>
2018-01-01 21:58:23 -0500

    Fix typo

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -11,7 +11,7 @@ class Owner
         today = Date.today
         birthday = Date.new(today.year, birthdate.month, birthdate.day)
         if birthday > today
-            countdown = (birthday - drtoday).to_i
+            countdown = (birthday - today).to_i
         else
             countdown = (birthday.next_year - today).to_i
         end
