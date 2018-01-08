42908a28bc71aae5effb6c40d414d26993f4139e
Travis Surgnier <surgie@gmail.com>
2018-01-07 20:34:04 -0500

    Production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,9 +62,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "learn-rails_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # ActionMailer config
+  config.action_mailer.default_url_options = { :host => Figaro.env.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: Figaro.env.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Figaro.env.gmail_username,
+    password: Figaro.env.gmail_password
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
